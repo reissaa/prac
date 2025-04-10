8a20fc6ab9723242b1df9b3199f7fb13f7e7ad0a
reissaa <matsunaga120613@gmail.com>
2025-04-10 11:40:46 +0900

    Update project.py

--- pages/project.py
+++ pages/project.py
@@ -28,9 +28,30 @@ img3=Image.open(Rf"wind/{site}_windrose.png")
 with col3:
    st.header("風配図")
    st.image(img3)
+Month=['January',
+ 'February',
+ 'March',
+ 'April',
+ 'May',
+ 'June',
+ 'July',
+ 'August',
+ 'September',
+ 'October',
+ 'November',
+ 'December']
 wepc=pd.read_csv(Rf"地点データ/{site}/wep_base/MWEPc({site}・省エネ).csv")
 weph=pd.read_csv(Rf"地点データ/{site}/wep_base/MWEPh({site}・省エネ).csv")
 wept=pd.read_csv(Rf"地点データ/{site}/wep_base/MWEPt({site}・省エネ).csv")
-st.dataframe(wepc, height=150)
-st.dataframe(weph, height=150)
-st.dataframe(wept, height=150)
+wepc_data=wepc.T
+wepc_data.columns=Month
+weph_data=wepc.T
+weph_data.columns=Month
+wept_data=wepc.T
+wept_data.columns=Month
+st.title(f'wepc 省エネ基準 地点:{site}')
+st.dataframe(wepc_data, height=150)
+st.title(f'weph 省エネ基準地点:{site}')
+st.dataframe(weph_data, height=150)
+st.title(f'wept 省エネ基準 地点:{site}')
+st.dataframe(wept_data, height=150)
